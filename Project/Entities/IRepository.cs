75a066f6c3a80de5600aaef43f85493f941327b2
Dimitrije Nestorov <dimitrijenestorov@hotmail.com>
2018-05-20 20:33:43 +0200

    Added database model and database interface.

---
+++ Project/Entities/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public interface IRepository<T,V>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(V id);
+        bool Insert(T entity);
+        bool Delete(V id);
+        bool Update(T entity);
+    }
+}
---
+++ Project/Entities/Models/PowerConsumptionData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class PowerConsumptionData
+    {
+        public DateTime Timestamp { get; set; }
+        public double Consumption { get; set; }
+        public string GeoAreaId { get; set; }
+    }
+}
