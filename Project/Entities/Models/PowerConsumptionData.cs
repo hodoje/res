54c1fc1ad5b8dc4d393c9a31a583b78dd1b91dfb
hodoje <nikolakaraklic@gmail.com>
2018-05-22 00:28:09 +0200

    Added EntityFramework to DataAccess and Entites. Added models and annotations.

--- Project/Entities/Models/PowerConsumptionData.cs
+++ Project/Entities/Models/PowerConsumptionData.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Entities.Models
 {
     public class PowerConsumptionData
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "datetime2")]
         public DateTime Timestamp { get; set; }
+        [Required]
         public double Consumption { get; set; }
+        [Required]
         public string GeoAreaId { get; set; }
+        [ForeignKey("GeoAreaId")]
+        public GeoArea GeoArea { get; set; }
     }
 }
