4e12d8b9dbae52ae55dcd0fcb50abba317e33807
Dimitrije Nestorov <dimitrijenestorov@hotmail.com>
2018-05-23 20:45:21 +0200

    Minor changes aplied to DataAcces and removed InitialMigrations migrations.

--- Project/DataAccess/PowerConsumptionDbContextProvider.cs
+++ Project/DataAccess/PowerConsumptionDbContextProvider.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class DbContextProvider : IDbContextProvider<PowerConsumptionDbContext>
+    public class PowerConsumptionDbContextProvider : IDbContextProvider<PowerConsumptionDbContext>
     {
         public PowerConsumptionDbContext GetDbContext()
         {
