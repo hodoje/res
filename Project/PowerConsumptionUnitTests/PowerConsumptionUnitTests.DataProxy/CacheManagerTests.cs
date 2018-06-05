a407214827c635bb073f9a4a7fce7871b05db3b2
Dimitrije Nestorov <dimitrijenestorov@hotmail.com>
2018-06-06 00:25:05 +0200

    Added folder for DataProxy unit tests.

---
+++ Project/PowerConsumptionUnitTests/PowerConsumptionUnitTests.DataProxy/CacheManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using DataProxy;
+using Entities.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerConsumptionUnitTests.PowerConsumptionUnitTests.DataProxy
+{
+    [TestFixture]
+    class CacheManagerTests
+    {
+        [Test]
+        public void CacheManager_PropertyReturnType_ObjectCache()
+        {
+            var result = ICacheManager<PowerConsumptionData>
+        }
+    }
+}
